6bd3fcfccdf2c9cdfb20fc39cc2497fee5bdaa44
brahim bouchikha <brahim.bouchikha@gmail.com>
2023-06-17 04:25:17 -0700

    print comb3

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - print all possible different combinations of two digits
+ * Return: 0
+ */
+int main(void)
+{
+	int i, j;
+	
+	for (i = 0; i < 9; i++)
+	{
+		for (j = i+1; j <= 9; j++)
+		{
+			putchar('0' + i);
+		       	putchar('0' + j);
+			if(i <= 8)
+				putchar(',');	
+			if (j>1)
+			{
+				putchar(' ');
+			}
+		}
+	}
+
+	return: (0)
+}
