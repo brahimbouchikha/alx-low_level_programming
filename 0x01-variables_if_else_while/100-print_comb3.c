d4b381f2318b2a7e806e6f297f8f33d8f7ae9640
brahim bouchikha <brahim.bouchikha@gmail.com>
2023-06-17 04:40:15 -0700

    print comb3

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -6,22 +6,22 @@
  */
 int main(void)
 {
-        int i, j;
-	
-	for (i = 0; i < 9; i++)
-        {
-                for (j = i+1; j <= 9; j++)
-                {
-                	putchar('0' + i);
-                	putchar('0' + j);
-                        if(i < 8)
-                        	putchar(',');
-                	if (j>=1)
-                	{
-                        	putchar(' ');
-                	}
-                }
-        }
+	int i, j;
 
+	for (i = 0; i < 9; i++)
+	{
+		for (j = i+1; j <= 9; j++)
+		{
+			putchar('0' + i);
+			putchar('0' + j);
+			if(i < 8)
+				putchar(',');
+			if (j>=1)
+			{
+			putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
         return (0);
 }
