5e18fcf2bbfa781cdf707992844115b8668db33a
brahim bouchikha <brahim.bouchikha@gmail.com>
2023-06-17 06:52:34 -0700

    102-print_comb5.c

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+ * main - print all possible combin of two two digit numbers
+ * Return: 0
+ */
+int main(void)
+{
+	int i, j, k, l;
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 9; j++)
+		{
+			for (k = 0; k < 10; k++)
+			{
+				for (l = 1; l < 10; l++)
+				{
+					if (i != k && j != l)
+					{
+						putchar('0' + i);
+						putchar('0' + j);
+						putchar(' ');
+						putchar('0' + k);
+						putchar('0' + l);
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
