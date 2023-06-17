cf8a5ec72bf07caac82d11181162a357662ade64
brahim bouchikha <brahim.bouchikha@gmail.com>
2023-06-16 18:17:34 -0700

    alphABET

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - print all letter exept q and e
+ * Return:0
+ */
+int main (void)
+{
+	char c;
+
+	c = a;
+	while (c <= 'z') 
+	{
+		if (c != 'q' && c != 'e')
+			putchar(c);
+		c++;
+	}
+	putchar('\n');
+	retrun(0);
+}
