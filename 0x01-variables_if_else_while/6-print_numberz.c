f606be608e101617725697ae14b3a8a0fefb7988
brahim bouchikha <brahim.bouchikha@gmail.com>
2023-06-16 18:54:42 -0700

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - print all single difit number of base 10
+ * Retrun: 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar('0' +i);
+	}
+	putchar('\n');
+	return (0);
+}
