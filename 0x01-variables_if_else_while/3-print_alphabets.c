f8d6d6ec668cc392177a1dff0ac11653a4397542
brahim bouchikha <brahim.bouchikha@gmail.com>
2023-06-16 18:31:53 -0700

    alphABET

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -10,12 +10,12 @@ int main(void)
 
 	lc = 'a';
 	uc = 'A';
-	while (lc < 'z')
+	while (lc <= 'z')
 	{
 		putchar(lc);
 		lc++;
 	}
-	while (uc < 'Z')
+	while (uc <= 'Z')
 	{
 		putchar(uc);
 		uc++;
