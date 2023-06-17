18581afb56b99711697da6589d0c6a53dc721a89
brahim bouchikha <brahim.bouchikha@gmail.com>
2023-06-16 18:30:06 -0700

    alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/*
+ * main - write aphabet in lowercase and uppercase
+ * Return: 0
+ */
+int main(void)
+{
+	char lc,uc;
+
+	lc = 'a';
+	uc = 'A';
+	while (a < 'z')
+	{
+		putchar(lc);
+		lc++;
+	}
+	while (A < 'Z')
+	{
+		putchar(uc);
+		uc++;
+	}
+	putchar('\n');
+	return (0);
+}
