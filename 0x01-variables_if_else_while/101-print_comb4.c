5689614c1277d2360376078930af82f73df0f3a6
brahim bouchikha <brahim.bouchikha@gmail.com>
2023-06-17 06:10:29 -0700

    pint comb 4

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -21,7 +21,42 @@ int main(void)
 				{			
 					putchar(',');
 					if (j >= 1 && k >= 2)
-						putchar(' ');
+				#include <stdio.h>
+
+/**
+ * main - print all possible different combinations of three digits
+ * Return:0
+ */
+int main(void)
+{
+        int i, j, k;
+
+        for (i = 0; i < 9; i++)
+        {
+                for (j = i + 1; j < 9; j++)
+                {	
+			if ( j > 1)
+			{
+				putchar(',');
+				putchar(' ';)
+                        for (k = j + 1; k < 10; k++)
+                        {
+                                putchar('0' + i);
+                                putchar('0' + j);
+                                putchar('0' + k);
+                                if (k < 9)
+				{
+                                        putchar(',');
+                                        if (j >= 1 && k >= 2)
+                                                putchar(' ');
+                                }
+                        }
+                }
+        }
+        putchar('\n');
+        return (0);
+}
+		putchar(' ');
 				}
 			}
 		}
