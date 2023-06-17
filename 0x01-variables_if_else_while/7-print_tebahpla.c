5f6e306f0d87377633509414857e0f149ecbe6f7
brahim bouchikha <brahim.bouchikha@gmail.com>
2023-06-16 19:01:06 -0700

    Smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - print lowercase alphabet in reverse
+ * Retrun:0
+ */
+int main(void)
+{
+	char i;
+
+
+	for (i = 'z'; z >= a ; z--)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+} 
