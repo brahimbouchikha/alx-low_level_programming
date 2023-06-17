f4660c6a250d975be3a820847632d7ee78494571
brahim bouchikha <brahim.bouchikha@gmail.com>
2023-06-16 19:12:25 -0700

    Smile in the mirror

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -7,11 +7,13 @@
 
 int main(void)
 {
-	for (int i = 0; i < 10; i++)
+	int i;
+
+	for (i = 0; i < 10; i++)
 	{
 		putchar('0' + i);
 	}
-	for (int i = 0; i < 6; i++)
+	for (i = 0; i < 6; i++)
 	{
 		putchar('a' + i);
 	}
