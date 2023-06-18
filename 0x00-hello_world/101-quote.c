55706359f7e7536f72b7661cafb138d132252aed
brahim bouchikha <brahim.bouchikha@gmail.com>
2023-06-18 06:17:18 -0700

    intel

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,10 +7,9 @@
 
 int main(void)
 {
-	char str[];
 	int len;
 
-	str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+	char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
 	len = sizeof(str) - 1;
 	write(1, str, len);
 	return (1);
