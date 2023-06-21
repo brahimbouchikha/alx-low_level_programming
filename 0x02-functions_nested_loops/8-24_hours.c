a21c575551235adb791ebe49dbdd19335d6eecfe
brahim bouchikha <brahim.bouchikha@gmail.com>
2023-06-20 18:26:45 -0700

    24 hours

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -6,7 +6,7 @@
 
 void jack_bauer(void)
 {
-	int hrs,min;
+	int hrs, min;
 
 	for (hrs = 0; hrs < 24; hrs++)
 	{
@@ -15,7 +15,7 @@ void jack_bauer(void)
 			_putchar('0' + hrs / 10);
 			_putchar('0' + hrs % 10);
 			_putchar(':');
-			_putchar('0' + min % 10);
+			_putchar('0' + min / 10);
 			_putchar('0' + min % 10);
 			_putchar('\n');
 		}
