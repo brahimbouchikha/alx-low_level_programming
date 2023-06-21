1da54b8e4d96b60261941d9aac0471b462ccba36
brahim bouchikha <brahim.bouchikha@gmail.com>
2023-06-21 13:34:02 -0700

    times tables

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -30,7 +30,7 @@ void print_times_table(int n)
 					_putchar('0' + prod / 100);
 					_putchar('0' + prod / 10);
 				}
-				_putchar('0' + (prod % 10))
+				_putchar('0' + (prod % 10));
 				if (j > 0 && j < n)
 				{
 					_putchar(',');
