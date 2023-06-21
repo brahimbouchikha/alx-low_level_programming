ef5ab2bc22a8376d6d7e2ced5a1f8d1514769828
brahim bouchikha <brahim.bouchikha@gmail.com>
2023-06-20 17:05:38 -0700

    sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,11 +1,12 @@
 #include "main.h"
+
 /**
  * print_sign - print the sign of a number
  * @n: number that will be chicked
  * Return: 1 if positive,0 if is 0, -1 otherwise
  */
 
-int print_sing(int n)
+int print_sign(int n)
 {
 	if (n > 0)
 	{
