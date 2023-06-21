04ba9c6457d5ca440f5c102a1ed6902ae3ec0deb
brahim bouchikha <brahim.bouchikha@gmail.com>
2023-06-20 19:18:42 -0700

    time table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -12,12 +12,12 @@ void times_table(void)
 	{
 		for (j = 0; j <  10; j++)
 		{	prod = i * j;
-			if(prod <= 9)
+			if (prod <= 9)
 				_putchar(' ');
 			else
 				_putchar('0' + prod / 10);
 			_putchar('0' +  (prod % 10));
-			if (j <= 8)
+			if (j <= 9)
 			{
 				_putchar(',');
 				_putchar(' ');
