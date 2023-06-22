5696e54951fde32543fc43aa40dbf65f92144c4d
brahim bouchikha <brahim.bouchikha@gmail.com>
2023-06-22 12:30:53 -0700

    is upper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,14 +2,15 @@
 
 /**
  * _isupper - check for upercase character.
- * @c : character input
- * Return : 1 if c is uppercase, 0 otherwise
+ * @c: character input
+ * Return: 1 if c is uppercase, 0 otherwise
  */
 
 int _isupper(int c)
 {
 	if (c >= 65 || c <= 90)
+	{
 		return (1);
-
+	}
 	return (0);
 }
