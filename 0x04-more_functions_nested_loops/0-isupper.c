00ae2947cccd801b3c77758383d812cbc5129e51
brahim bouchikha <brahim.bouchikha@gmail.com>
2023-06-22 12:27:43 -0700

    is upper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,8 +2,8 @@
 
 /**
  * _isupper - check for upercase character.
- * @c: character input
- * Return: 1 if c is uppercase, 0 otherwise
+ * @c : character input
+ * Return : 1 if c is uppercase, 0 otherwise
  */
 
 int _isupper(int c)
