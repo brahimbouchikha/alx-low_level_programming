e025a502baade0b463f096f44f83ec48404db657
brahim bouchikha <brahim.bouchikha@gmail.com>
2023-06-22 12:51:44 -0700

    multiple

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * mul - multiples two integers
+ * @a: first input integer
+ * @b: second input integre
+ * Return: result of multiple
+ */
+
+
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
