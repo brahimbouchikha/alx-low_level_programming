f4d2a0f35522e488389dacafa0ad498db3d73f10
brahim bouchikha <brahim.bouchikha@gmail.com>
2023-06-23 13:21:52 -0700

    fizz buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -12,14 +12,14 @@ int main(void)
 	printf("1 2");
 	for (i = 3; i <= 100; i++)
 	{
-		if (i % 3 == 0)
-			printf(" Fizz");
+		if (i % 3 == 0 && i % 5 == 0)
+			printf(" FizzBuzz");
 
 		else if (i % 5 == 0)
 			printf(" Buzz");
 
-		else if ((i % 3 == 0) && (i % 5 == 0))
-			printf(" FizzBuzz");
+		else if (i % 3 == 0)
+			printf(" Fizz");
 
 		else
 			printf(" %d", i);
