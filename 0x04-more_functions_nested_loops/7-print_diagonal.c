88b430acbe48a4b8d623ea3a398bcc73b787a391
brahim bouchikha <brahim.bouchikha@gmail.com>
2023-06-23 12:46:21 -0700

    print_diagonal

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -15,13 +15,12 @@ void print_diagonal(int n)
 	}
 	else
 	{
-		for (i = 0; i < n; i++) 
+		for (i = 0; i < n; i++)
 		{
 			for (j = 0; j < i; j++)
 			{
 				_putchar(32);
 			}
-
 			_putchar(92);
 			_putchar('\n');
 		}
