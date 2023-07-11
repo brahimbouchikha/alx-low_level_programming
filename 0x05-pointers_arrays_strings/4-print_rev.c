f954bf1506017a61bf4a85577d821852c619f843
brahim bouchikha <brahim.bouchikha@gmail.com>
2023-07-11 14:37:34 -0700

    4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -2,7 +2,7 @@
 
 /**
  * print_rev - print_rev
- * s: input string
+ * @s: input string
  */
 
 void print_rev(char *s)
@@ -13,9 +13,11 @@ void print_rev(char *s)
 	{
 		n++;
 	}
+
 	for (i = n - 1; i >= 0 ; i--)
 	{
 		_putchar(s[i]);
 	}
+
 	_putchar('\n');
 }
