eee194ed5cd54dc90bb0ae44d7371180034ca0ce
brahim bouchikha <brahim.bouchikha@gmail.com>
2023-07-12 14:41:54 -0700

    3

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -15,9 +15,9 @@ int **alloc_grid(int width, int height)
 
 	if (width == 0 || height == 0)
 		return (NULL);
-	tab = malloc(sizeof(int) * height);
+	tab = malloc(sizeof(int *) * height);
 
-	if(tab == NULL)
+	if (tab == NULL)
 	{
 		free(tab);
 		return (NULL);
