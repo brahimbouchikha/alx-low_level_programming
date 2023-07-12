190282360bb498a413891d9c9595d5cad9b3ea90
brahim bouchikha <brahim.bouchikha@gmail.com>
2023-07-11 17:34:33 -0700

    2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -13,9 +13,9 @@ char *str_concat(char *s1, char *s2)
 	int n, m, i = 0, j = 0, len;
 	char *tab;
 
-	if(s1 == NULL)
+	if (s1 == NULL)
 		s1 = "";
-	if(s2 == NULL)
+	if (s2 == NULL)
 		s2 = "";
 
 	while (s1[i])
