5fad59ddc47036cb8ff912e28043c2450b636ffa
brahim bouchikha <brahim.bouchikha@gmail.com>
2023-07-12 18:17:50 -0700

    100 change

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -4,7 +4,7 @@
 
 /**
  * main - prints the minimum number of coins to make 
- * 	change for an amount of money
+ * 		change for an amount of money
  * @argc: number of arguments
  * @argv: Arguments
  * Return: 1 if number of arguments passed to your program is not exactly
@@ -15,42 +15,39 @@ int main(int argc, char *argv[])
 {
 	int coins = 0, cents;
 
-	if (argc == 2)
-	{
-		cents = atoi(argv[1]);
-		while (cents > 0)
-		{
-			coins ++;
-			if ((cents - 25) >= 0)
-			{
-				cents -= 25;
-				continue;
-			}
-			if ((cents - 10) >= 0)
-			{
-				cents -= 10;
-				continue;
-			}
-			if ((cents - 5) >= 0)
-			{
-				cents -= 5;
-				continue;
-			}
-			if ((cents - 2) >= 0)
-			{
-				cents -= 25;
-				continue;
-			}
-			cents--;
-		}
-		printf("%d\n", coins);
-	}
-	else
+	if (argc != 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
+	cents = atoi(argv[1]);
+	while (cents > 0)
+	{
+		coins ++;
+		if ((cents - 25) >= 0)
+		{
+			cents -= 25;
+			continue;
+		}
+		if ((cents - 10) >= 0)
+		{
+			cents -= 10;
+			continue;
+		}
+		if ((cents - 5) >= 0)
+		{
+			cents -= 5;
+			continue;
+		}
+		if ((cents - 2) >= 0)
+		{
+			cents -= 25;
+			continue;
+		}
 	
+		cents--;
+	}
+		printf("%d\n", coins);
 
 	return (0);
 }
