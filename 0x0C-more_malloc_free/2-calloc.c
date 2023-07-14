a01c54891e7920eae918da9a276e7fbe55c996e3
brahim bouchikha <brahim.bouchikha@gmail.com>
2023-07-14 07:52:23 -0700

    2

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -6,6 +6,7 @@
  * _calloc - allocates memory for an array, using malloc
  * @nmemb: number of elements
  * @size: number of bytes
+ * Return: nothing
  */
 
 void *_calloc(unsigned int nmemb, unsigned int size)
@@ -15,9 +16,10 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
-	len = nmemb * size;
 
+	len = nmemb * size;
 	ptr = malloc(len);
+
 	if (ptr == NULL)
 		return (NULL);
 
