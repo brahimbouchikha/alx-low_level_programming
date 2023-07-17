17d69ae61b6e5aa1067e7842a7ddfa251c9d6564
brahim bouchikha <brahim.bouchikha@gmail.com>
2023-07-17 16:51:59 -0700

    0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - a dog struct
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner
+ *
+ * Descrtiption: a long dog struct
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+
+#endif
