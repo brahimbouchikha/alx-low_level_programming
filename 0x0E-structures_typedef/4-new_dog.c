c0fa037d0a15ac12a8c7a767ea1003b7a0ad8ce8
brahim bouchikha <brahim.bouchikha@gmail.com>
2023-07-18 16:27:16 -0700

    4

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -71,8 +71,8 @@ dog_t *new_dog(char *name, float age, char *owner)
 		free(dog);
 		return (NULL);
 	}
-	dog->name = _strcopy(dog->name, name);
+	dog->name = _strcpy(dog->name, name);
 	dog->age = age;
-	dog->owner = _strcopy(dog->owner, owner);
+	dog->owner = _strcpy(dog->owner, owner);
 	return (dog);
 }
