a3ecd43b1c4b96543531b557e9d123aed06a89a0
brahim bouchikha <brahim.bouchikha@gmail.com>
2023-07-25 04:46:58 -0700

    0

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,9 +1,13 @@
 #include "main.h"
 #include <stdio.h>
+
+
+
 /**
  * _puts -  prints a string, followed by a new line, to stdout.
  * @str: input string
  */
+
 void _puts(char *str)
 {
 	for (; *str != '\0'; str++)
