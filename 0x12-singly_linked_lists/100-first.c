0468ec8e95db1a37d457795778ccca07d602956c
brahim bouchikha <brahim.bouchikha@gmail.com>
2023-07-27 19:07:12 -0700

    100

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,15 @@
+#include "list.h"
+
+void _constructor(void) __attribute__ ((constructor));
+
+/**
+ * _constructor - execute befor main()
+ *
+ * Return void
+ */
+
+void _constructor(void)
+{
+	printf("You're beat! and yet, you must allow,\n"
+			"I bore my house upon my back!\n");
+}
