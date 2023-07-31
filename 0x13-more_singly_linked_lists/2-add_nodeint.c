9363cca05087d5c32042fbc7efcafccd75250304
brahim bouchikha <brahim.bouchikha@gmail.com>
2023-07-31 15:24:35 -0700

    2

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -13,11 +13,11 @@ listint_t *add_nodeint(listint_t **head, const int n)
 	listint_t *ptr;
 
 	ptr = malloc(sizeof(listint_t));
+	if (!ptr)
+		return (NULL);
+
 	ptr->n = n;
 	ptr->next = *head;
-	if(!ptr)
-		return (NULL);
 	*head = ptr;
-
 	return (*head);
 }
