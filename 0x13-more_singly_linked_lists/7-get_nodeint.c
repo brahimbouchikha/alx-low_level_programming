e69d1a41fcc41d53893344ad9003da59d87b6805
brahim bouchikha <brahim.bouchikha@gmail.com>
2023-08-01 16:52:55 -0700

    7

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -19,8 +19,8 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 		ptr = ptr->next;
 	}
 
-	if (!ptr)
+	if(!ptr)
 		return (NULL);
-	else
-		return (ptr);
+	
+	return (ptr);
 }
