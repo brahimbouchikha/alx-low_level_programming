b96ec6861e1c90656a569a4e3f0b469250468d9d
brahim bouchikha <brahim.bouchikha@gmail.com>
2023-08-01 17:37:41 -0700

    9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -11,31 +11,32 @@
 
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	 unsigned int i;
-	 listint_t *new;
-	 listint_t *ptr = *head;
+	unsigned int i;
+	listint_t *new;
+	listint_t *ptr = *head;
 
-	 new = malloc(sizeof(listint_t));
-	 if (!new || !head)
-		 return (NULL);
-	 new->n = n;
-	 new->next = NULL;
-	 if (idx == 0)
-	 {
-		 new->next = *head;
-		 *head = new;
-		 return (new);
-	 }
-	 for (i = 0; ptr && i < idx; i++)
-	 {
-		 if (i == idx - 1)
-		 {
-			 new->next = ptr->next;
-			 ptr->next = new;
-			 return (new);
-		 }
-		 else
-			 ptr = ptr->next;
-	 }
-	 return (NULL);
+	new = malloc(sizeof(listint_t));
+
+	if (!new || !head)
+		return (NULL);
+	new->n = n;
+	new->next = NULL;
+	if (idx == 0)
+	{
+		new->next = *head;
+		*head = new;
+		return (new);
+	}
+	for (i = 0; ptr && i < idx; i++)
+	{
+		if (i == idx - 1)
+		{
+			new->next = ptr->next;
+			ptr->next = new;
+			return (new);
+		}
+		else
+			ptr = ptr->next;
+	}
+	return (NULL);
 }
