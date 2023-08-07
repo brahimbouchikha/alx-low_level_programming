44e0c9562188a87d1b94cb73254bb0e8b4f6f5fe
brahim bouchikha <brahim.bouchikha@gmail.com>
2023-08-07 16:50:10 -0700

    1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -20,8 +20,8 @@ int create_file(const char *filename, char *text_content)
 		for (len = 0; text_content[len]; )
 			len++;
 	}
-	fd = open(filename, O_CREAT | O_RDWR | O_TRUNCT, 0600);
-	_write = (fd, text_content, len);
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	_write = write(fd, text_content, len);
 	if (fd == -1 || _write == -1)
 		return (-1);
 	close(fd);
