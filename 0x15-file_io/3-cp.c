e4f6b52dd78e5bae264957fcd97eb1798c182291
brahim bouchikha <brahim.bouchikha@gmail.com>
2023-08-08 16:46:53 -0700

    3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -60,7 +60,7 @@ int main(int argc, char *argv[])
 	do {
 		if (file_from == -1 || r == -1)
 		{
-			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n",argv[1]);
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 			exit(98);
 			free(buffer);
 		}
