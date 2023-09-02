f4a101ff208d52fbb844e8232955b41624ad60e9
brahim bouchikha <brahim.bouchikha@gmail.com>
2023-09-01 17:40:53 -0700

    0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - print program name fllowed by new line.
+ * @argc: number of arguments
+ * @argv: arguments
+ *
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s", argv[0]);
+	return (0);
+}
