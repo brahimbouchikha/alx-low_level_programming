b9bcc5b99e1f2838a6bcec7f1326deea5cb82036
brahim bouchikha <brahim.bouchikha@gmail.com>
2023-09-04 15:10:03 -0700

    100

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -10,42 +10,42 @@
 
 int main(int argc, char *argv[])
 {
-	int count = 0, cents;
-
+	int count, cents;
+	
 	if (argc != 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
 	cents = atoi(argv[1]);
-		while (cents > 0)
+	count = 0;
+	while (cents > 0)
+	{
+		count++;
+		if (cents - 25 >= 0)
 		{
-			count++;
-			if (cents - 25 >= 0)
-			{
-				cents -= 25;
-				countinue;
-			}
-			if (cents - 10 >= 0)
-			{
-				cents -= 10;
-				countinue;
-			}
-			if (cents - 5 >= 0)
-			{
-				cents -= 5;
-				countinue;
-			}
-			if (cents - 2 >= 0)
-			{
-				cents -= 2;
-				countinue;
-			}
-			if (cents - 1 >= 0)
-			{
-				cents -= 1;
-				countinue;
-			}
+			cents -= 25;
+			continue;
+		}
+		if (cents - 10 >= 0)
+		{
+			cents -= 10;
+			continue;
+		}
+		if (cents - 5 >= 0)
+		{
+			cents -= 5;
+			continue;
+		}
+		if (cents - 2 >= 0)
+		{
+			cents -= 2;
+			continue;
+		}
+		if (cents - 1 >= 0)
+		{
+			cents -= 1;
+			continue;
 		}
 	}
 
