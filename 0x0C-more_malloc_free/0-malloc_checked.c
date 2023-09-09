8f5cb2c46386491ceb91e01817723724277f9c9f
brahim bouchikha <brahim.bouchikha@gmail.com>
2023-09-08 17:57:44 -0700

    0. Trust no one

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,21 @@
+#include "main.h" 
+
+/**
+ * malloc_checked - function that allocates memory using
+ * @b: input size to allocate
+ *
+ * Return: nothing
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	void *p;
+
+	p = malloc(b);
+	if (p == NULL)
+	{
+		exit(98);
+	}
+
+	return (p);
+}
