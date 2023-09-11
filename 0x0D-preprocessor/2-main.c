acd85c4937458587fbc76944818713334bf47c71
brahim bouchikha <brahim.bouchikha@gmail.com>
2023-09-11 15:02:40 -0700

    2. File name

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
 
 /**
- * main - prints the name of the file it was 
- *	compiled from, followed by a new line
+ * main - prints the name of the file it was
+ *compiled from, followed by a new line
  *
- * Return: 0
  */
-int main()
+void main(void)
 {
 	printf("%s\n", __FILE__);
-	return (0);
 }
