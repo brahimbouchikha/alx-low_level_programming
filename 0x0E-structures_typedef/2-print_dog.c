3708d053a41a6af75ba170b6d16888a9ce321a23
brahim bouchikha <brahim.bouchikha@gmail.com>
2023-09-12 15:51:08 -0700

    gcc -Wall -pedantic -Werror -Wextra -std=gnu89 2-main.c 2-print_dog.c -o c

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -8,17 +8,8 @@ void print_dog(struct dog *d)
 {
 	if (d != NULL)
 	{
-		if ((*d).name == NULL)
-			printf("Name: (nil)\n");
-		else
-			printf("Name: %s\n", (*d).name);
-		if ((*d).age == 0)
-			printf("Name: (nil)\n");
-		else
-			printf("Age: %f\n", (*d).age);
-		if ((*d).owner == NULL)
-			printf("Name: (nil)\n");
-		else
-			printf("Owner: %s\n", (*d).owner);
+		printf("Name: %s", ((*d).name == NULL) ? "(nil)" : (*d).name);
+		printf("Age: %f", (*d).age);
+		printf("Owner: %s", ((*d).owner == NULL) ? "(nil)" : (*d).owner);
 	}
 }
