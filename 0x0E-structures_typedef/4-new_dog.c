e75100dc7a3ca4eba87baa088d9f9a927029467f
brahim bouchikha <brahim.bouchikha@gmail.com>
2023-09-12 17:28:39 -0700

    4. A door is what a dog is perpetually on the wrong side of

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,6 +1,23 @@
 #include "dog.h"
 
 /**
+ * _str_len - Calculate the length of string
+ * @str: pointer to string
+ *
+ * Return: lenght of string
+ */
+
+size_t _str_len(char *str)
+{
+	size_t i = 0;
+
+	while (str[i] != '\0')
+		i++;
+
+	return (i);
+}
+
+/**
  * new_dog - creates a new dog
  * @name: new dog's name
  * @age: new dog's age
@@ -11,14 +28,33 @@
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
+	size_t i, name_len, owner_len;
 	dog_t *ptr;
 
 	ptr = malloc(sizeof(dog_t));
 	if (ptr == NULL)
 		return (NULL);
-	if (name != NULL)
-		ptr->name = name;
+	name_len = _str_len(name);
+	owner_len = _str_len(name);
+
+	ptr->name = malloc(name_len + 1);
+	ptr->owner = malloc(owner_len + 1);
+
+	if (ptr->name == NULL || ptr->owner == NULL)
+	{
+		free(ptr->name);
+		free(ptr->owner);
+		free(ptr);
+		return (NULL);
+	}
+	for (i = 0; i <= name_len; i++)
+	{
+		ptr->name[i] = name[i];
+	}
+	for (i = 0; i <= owner_len; i++)
+	{
+		ptr->owner[i] = owner[i];
+	}
 	ptr->age = age;
-	ptr->owner = owner;
 	return (ptr);
 }
