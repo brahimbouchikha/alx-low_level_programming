a2eb489365f9e65687a80316b99242e08fcb163b
brahim bouchikha <brahim.bouchikha@gmail.com>
2023-09-13 17:34:09 -0700

    3. A goal is not always meant to be reached, it often serves simply as something to aim at

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,28 @@
+#include "function_pointers.h"
+
+
+ /**
+ * int_index - Searches for an integer in an array using a comparison function.
+ * @array: Pointer to the integer array to search.
+ * @size: Size of the array.
+ * @cmp: Pointer to a comparison function that
+ *	takes an integer as its argument.
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (array == NULL || cmp == NULL)
+		return (-1);
+
+	if (size <= 0)
+		return (-1);
+
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i]) != 0)
+			return (i);
+	}
+	return (-1);
+}
