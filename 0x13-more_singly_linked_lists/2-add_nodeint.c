15cea9c2e377b0e6ea4693f5ed54877ac114425d
brahim bouchikha <brahim.bouchikha@gmail.com>
2023-09-26 15:19:47 -0700

    2. Add node

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -17,7 +17,7 @@ listint_t *add_nodeint(listint_t **head, const int n)
 		return (NULL);
 	ptr->n = n;
 	ptr->next = NULL;
-	ptr->next = *head;	
+	ptr->next = *head;
 	*head = ptr;
 	return (*head);
 }
