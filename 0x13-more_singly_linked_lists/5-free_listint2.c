3ce84219c78ddcb2dbe0ce27b9c8974b01fd162c
brahim bouchikha <brahim.bouchikha@gmail.com>
2023-09-26 17:26:27 -0700

    5. Free

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * free_listint22 - frees a listint_t list
+ * @head: pointer to the linked list
+ *
+ * Return: Nothing
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *ptr, *tmp;
+
+	ptr = *head;
+
+	while (ptr != NULL)
+	{
+		tmp = ptr->next;
+		free(ptr);
+		ptr = tmp;
+	}
+	*head = NULL;
+}
