726aa2b36fe8ed1cb65b95487ebe94665df90529
brahim bouchikha <brahim.bouchikha@gmail.com>
2023-09-29 12:18:50 -0700

    1

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_binary - prints the binary representation of a number
+ * @n: number to convert
+ */
+
+void print_binary(unsigned long int n)
+{
+	int bitCount = 0, temp = n;
+
+	while (temp > 0)
+	{
+	temp = temp >> 1;
+	bitCount++;
+	}
+	while (bitCount > 0)
+	{
+	printf("%lu", (n >> (bitCount - 1)) & 1);
+	bitCount--;
+	}
+}
