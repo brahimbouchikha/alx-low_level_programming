ef75d0bcb29b7fda9e2ee0f68662c5e98a69c5d3
brahim bouchikha <brahim.bouchikha@gmail.com>
2023-09-29 12:32:17 -0700

    1

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -7,16 +7,17 @@
 
 void print_binary(unsigned long int n)
 {
-	int bitCount = 0, temp = n;
+	int bitCount = 0;
+	unsigned long int temp = n;
 
 	while (temp > 0)
 	{
-	temp = temp >> 1;
-	bitCount++;
+		temp = temp >> 1;
+		bitCount++;
 	}
 	while (bitCount > 0)
 	{
-	printf("%lu", (n >> (bitCount - 1)) & 1);
-	bitCount--;
+		printf("%lu", (n >> (bitCount - 1)) & 1);
+		bitCount--;
 	}
 }
