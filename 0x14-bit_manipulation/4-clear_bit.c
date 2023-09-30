d27eab0ff4e8d071584bf41053d4df825ba0a8da
brahim bouchikha <brahim.bouchikha@gmail.com>
2023-09-29 18:08:19 -0700

    101

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -10,31 +10,31 @@
 
 int clear_bit(unsigned long int *n, unsigned int index)
 {
-        unsigned long int temp = *n, d, res = 0;
-        unsigned int i, bitcount;
+	unsigned long int temp = *n, d, res = 0;
+	unsigned int i, bitcount;
 
-        if (*n == 0)
-        {
-                *n = 1 << index;
-                return (1);
-        }
-        while (temp > 0)
-        {
-                temp = temp >> 1;
-                bitcount++;
-        }
-        if (index < bitcount)
-        {
-                i = bitcount - 1;
-                while (bitcount > 0)
-                {
-                        d = (i - index == 0) ? 0 : (*n >> (bitcount - 1)) & 1;
-                        res = (res << 1) | d;
-                        i--;
-                        bitcount--;
-                }
-                *n = res;
-                return (1);
-        }
-        return (-1);
+	if (*n == 0)
+	{
+		*n = 1 << index;
+		return (1);
+	}
+	while (temp > 0)
+	{
+		temp = temp >> 1;
+		bitcount++;
+	}
+	if (index < bitcount)
+	{
+		i = bitcount - 1;
+		while (bitcount > 0)
+		{
+			d = (i - index == 0) ? 0 : (*n >> (bitcount - 1)) & 1;
+			res = (res << 1) | d;
+			i--;
+			bitcount--;
+		}
+		*n = res;
+		return (1);
+	}
+	return (-1);
 }
