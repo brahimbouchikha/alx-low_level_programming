38214f4ff98978dcfe9cc99f4fdfff195dcd8950
brahim bouchikha <brahim.bouchikha@gmail.com>
2023-10-02 17:24:07 -0700

    0. Tread lightly, she is near

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,6 +1,5 @@
 #include "main.h"
 
-
 /**
  * read_textfile - reads a text file and prints it to the POSIX standard output
  * @filename: the file name to read
@@ -9,28 +8,30 @@
  * Return: actual number of letters it could read and print
  */
 
+
+
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	size_t bread, bwrite;
+	ssize_t bytes_read, bytes_written = 0;
 	int fd;
 	char *buffer;
 
-	if (!filename)
+	if (filename == NULL)
 		return (0);
-
-	fd = open(filename, 0_RDONLY);
+	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 		return (0);
 	buffer = malloc(sizeof(char) * letters);
 	if (!buffer)
 	{
-		close(fd);
 		free(buffer);
+		close(fd);
 		return (0);
 	}
-	bread = read(fd, buffer, letters);
-	bwrite = write(STDOUT_FILENO, buffer, bread);
+	bytes_read = read(fd, buffer, letters);
+	bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
+
 	free(buffer);
 	close(fd);
-	return (bwrite);
+	return (bytes_written);
 }
