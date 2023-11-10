a07f439cc52a252872fcf39b511d3342b96c2934
brahim bouchikha <brahim.bouchikha@gmail.com>
2023-11-10 04:31:02 -0800

    4

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * free_dlistint - frees a list
+ * @head: the head of the lists of free
+ *
+ */
+
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *ptr = NULL;
+
+	while(head != NULL)
+	{
+		ptr = head->next;
+		free(head);
+		head = ptr;
+	}
+}
