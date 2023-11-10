6f43bfa8649bdda2ed74ca032ec8ed5efd165235
brahim bouchikha <brahim.bouchikha@gmail.com>
2023-11-10 04:24:56 -0800

    3

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -2,10 +2,10 @@
 
 /**
  * add_dnodeint_end - adds a new node at the end of a list
- * @head: the head of the list that we want to add a node 
+ * @head: the head of the list that we want to add a node
  * @n: data if the new node
  *
- * the address of the new element, or NULL if it failed
+ * Return: the address of the new element, or NULL if it failed
  */
 
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
