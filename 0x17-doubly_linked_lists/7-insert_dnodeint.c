e1462ae4220944864249e747386756ce97663c22
brahim bouchikha <brahim.bouchikha@gmail.com>
2023-11-10 16:42:18 -0800

    7. Insert at index

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -12,7 +12,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	dlistint_t *node, *tmp = (*h), *tmp2;
 
-	if ((*head) = NULL)
+	if ((*h) == NULL)
 		return (NULL);
 	node = malloc(sizeof(dlistint_t));
 	if (node == NULL)
