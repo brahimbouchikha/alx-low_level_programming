0c2bc6dbfbe1fc68790a35503ad259b98c41c02c
brahim bouchikha <brahim.bouchikha@gmail.com>
2023-11-18 16:45:58 -0800

    7

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -17,10 +17,10 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		return (add_dnodeint(h, n));
 	while (idx != 1)
 	{
-		idx--;
 		tmp = tmp->next;
 		if (tmp == NULL)
 			return (NULL);
+		idx--;
 	}
 	if (tmp->next == NULL)
 		return (add_dnodeint_end(h, n));
