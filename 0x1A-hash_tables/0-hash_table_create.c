4f6bf6dd28b2cbe104e7015b42fd3861725f603a
brahim bouchikha <brahim.bouchikha@gmail.com>
2023-12-31 12:19:54 -0800

    0

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -7,7 +7,7 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *new_table;
-	
+
 	new_table = (hash_table_t *) malloc(sizeof(hash_table_t));
 	if (new_table == NULL)
 		return (NULL);
