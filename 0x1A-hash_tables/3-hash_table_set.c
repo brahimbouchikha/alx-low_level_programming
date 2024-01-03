918fe776925465fd5512bee66ce97bf0e076da0b
brahim bouchikha <brahim.bouchikha@gmail.com>
2024-01-03 14:35:36 -0800

    3

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,42 +1,54 @@
 #include "hash_tables.h"
+
+/**
+ * make_node - create a new hash node
+ * @key: the key of the hash node
+ * @value: the value of the hash node
+ * Return: the new hash node, or NULL on failure
+ */
+hash_node_t *make_node(const char *key, const char *value)
+{
+        hash_node_t *node = (hash_node_t *) malloc(sizeof(hash_node_t));
+
+        if (!node)
+                return (NULL);
+        node->value = strdup(value);
+        node->key = strdup(key);
+        if (!node->key || !node->value)
+        {
+                free(node->key);
+                free(node->value);
+                free(node);
+                return (NULL);
+        }
+ 	 node->next = NULL;
+        return (node);
+}
+
 /**
  * hash_table_set - function that adds an element to the hash table
  * @ht: the hash table you want to add or update the key/value to
  * @key: is the key. key can not be an empty string
  * @value: is the value associated with the key.
+  * @value: is the value associated with the key.
  * Return: 1 if it succeeded, 0 otherwise
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	unsigned long int index;
-	hash_node_t *entry;
+        unsigned long int index;
+        hash_node_t *entry ;
 
-	if (key == NULL)
-		return (0);
-	entry = (hash_node_t *) malloc(sizeof(hash_node_t));
+        if (!key || !value || !ht)
+                return (0);
+        entry = (hash_node_t *) malloc(sizeof(hash_node_t));
+        if (!entry)
+                return (0);
+        index = key_index((const unsigned char *)key, ht->size);
+	entry = make_node(key, value);
 	if (entry == NULL)
 		return (0);
-	entry->key = malloc(strlen(key) + 1);
-	entry->value = malloc(strlen(value) + 1);
-	if (entry->key == NULL || entry->value == NULL)
-	{
-		free(entry);
-		return (0);
-	}
-	entry->next = NULL;
-	strcpy(entry->key, key);
-	strcpy(entry->value, value);
-	index = hash_djb2((const unsigned char *)key);
-	if (ht->array[index] == NULL)
-	{
-		ht->array[index] = entry;
-		return (1);
-	}
-	else
-	{
-		entry->next = ht->array[index];
-		ht->array[index] = entry;
-		return (1);
-	}
-	return (0);
+	entry->next = ht->array[index];
+	ht->array[index] = entry;
+        return (1);
 }
+
