5f9e6453b83ebe7e9ee987fb801b408d374bba54
brahim bouchikha <brahim.bouchikha@gmail.com>
2024-01-04 07:06:54 -0800

    4

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -18,19 +18,17 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 		return (NULL);
 	index = key_index((const unsigned char *)key, ht->size);
 	ptr = ht->array[index];
-
-	current = ptr;
 	if (current != NULL)
 	{
-		/*current = ptr;
+		current = ptr;
 		while (current != NULL)
-		{*/
+		{
 			if (strcmp(current->key, key) == 0)
 			{
 				return (current->value);
 			}
 			current = current->next;
-		/* } */
+		}
 	}
 	return (NULL);
 }
