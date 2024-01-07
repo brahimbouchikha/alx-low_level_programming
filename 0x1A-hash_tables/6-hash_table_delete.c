969ed10359bbfdfa46ae30ad2f008b859d00c269
brahim bouchikha <brahim.bouchikha@gmail.com>
2024-01-07 03:10:14 -0800

    6

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,29 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_delete - function that deletes a hash table.
+ * @ht: the hash table
+ */
+void hash_table_delete(hash_table_t *ht)
+{
+	unsigned long int i;
+	hash_node_t *tmp, *current;
+
+	for (i = 0; i < ht->size; i++)
+	{
+		if (ht->array[i] != NULL)
+		{
+			current = ht->array[i];
+			while (current != NULL)
+			{
+				tmp = current;
+				free(tmp->key);
+				free(tmp->value);
+				free(tmp);
+				current = current->next;
+			}
+			free(ht->array);
+			free(ht);
+		}
+	}
+}
