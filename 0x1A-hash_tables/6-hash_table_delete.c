2ba217ae1ed9d9f854316d3060f1b632f41e732c
brahim bouchikha <brahim.bouchikha@gmail.com>
2024-01-07 14:39:51 -0800

    6

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -10,15 +10,15 @@ void hash_table_delete(hash_table_t *ht)
 	unsigned long int i;
 	hash_node_t *current;
 
-	if (!ht)
+	if (ht == NULL || ht->array == NULL || ht->size == 0)
 		return;
 	for (i = 0; i < ht->size; i++)
 	{
 		while (ht->array[i] != NULL)
 		{
 			current = ht->array[i]->next;
-			free(ht->array[i]->value);
 			free(ht->array[i]->key);
+			free(ht->array[i]->value);
 			free(ht->array[i]);
 			ht->array[i] = current;
 		}
